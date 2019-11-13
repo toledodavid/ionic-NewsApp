6cc64d643578bf049535c77f8387b7d1b8221ebb
David Toledo <dtoledoz0207@gmail.com>
2019-11-12 23:10:44 -0600

    Interface file created
    This adds the interface file to get a response type and have a easy
    way to get the elements of the response JSON.

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {RespuestaTopHeadlines} from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class NoticiasService {
   constructor(private http: HttpClient) { }
 
   getTopHeadlines(){
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=13543b43dd0f4d819b36fdda8d4cb071`);
+    return this.http.get<RespuestaTopHeadlines>(`https://newsapi.org/v2/top-headlines?country=us&apiKey=13543b43dd0f4d819b36fdda8d4cb071`);
   }
 
 }
