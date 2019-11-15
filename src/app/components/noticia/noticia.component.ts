dbdc31c260cfef484820e3050f4cd20114dfb600
David Toledo <dtoledoz0207@gmail.com>
2019-11-14 22:02:21 -0600

    Create noticias and noticia component
    This adds the noticias and noticia component to get more clear the code.

---
+++ src/app/components/noticia/noticia.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from '../../interfaces/interfaces';
+
+@Component({
+  selector: 'app-noticia',
+  templateUrl: './noticia.component.html',
+  styleUrls: ['./noticia.component.scss'],
+})
+export class NoticiaComponent implements OnInit {
+
+  @Input() noticia:Article;
+  @Input() i:number;  
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
