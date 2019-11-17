581ce1b9de01295142c5fc13d062b70776e04ff5
David Toledo <dtoledoz0207@gmail.com>
2019-11-17 09:20:06 -0600

    Add InAppBrowser plugin
    This adds the inAppBrowser plugin to open the url of the news in
    the phone browser.

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -11,8 +12,12 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia:Article;
   @Input() i:number;  
 
-  constructor() { }
+  constructor(private iab: InAppBrowser) { }
 
   ngOnInit() {}
+
+  abrirNoticia() {
+    const browser = this.iab.create(this.noticia.url, '_system');
+  }
 
 }
