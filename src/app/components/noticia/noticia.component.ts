dbca7fa7da112a5d6f06885ccc33d26592a4e502
David Toledo <dtoledoz0207@gmail.com>
2019-11-18 00:04:02 -0600

    Add ActionSheet in noticiaComponent
    This adds the ActionSheet in the noticiaComponent, this actionsheet
    contain the buttons to Compartir, Favoritos, and Cancelar.

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-noticia',
@@ -12,12 +13,43 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia:Article;
   @Input() i:number;  
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser, private actionSheetCtrl:ActionSheetController) { }
 
   ngOnInit() {}
 
   abrirNoticia() {
     const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      },
+      {
+        text: 'Favorito',
+        icon: 'star',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorito');
+        }
+      },
+      {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
