3f929c6cd1f662383e195e8a0c757cce4371a81d
David Toledo <dtoledoz0207@gmail.com>
2019-11-20 19:25:32 -0600

    animations-with-animate-css-code

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Article } from '../interfaces/interfaces';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class DataLocalService {
 
   noticias:Article[] = [];
 
-  constructor(private storage: Storage) { 
+  constructor(private storage: Storage, public toastCtrl: ToastController) { 
     this.cargar_noticias_favoritos();
   }
 
@@ -20,6 +21,7 @@ export class DataLocalService {
     if(!existe) {
       this.noticias.unshift(noticia);
       this.storage.set('favoritos', this.noticias);
+      this.presentToast('Agregado a favoritos');
     }
     
   }
@@ -36,5 +38,14 @@ export class DataLocalService {
   borrar_noticia_favoritos(noticia: Article) {
     this.noticias = this.noticias.filter(noti => noti.title !== noticia.title);
     this.storage.set('favoritos', this.noticias);
+    this.presentToast('Borrado de favoritos');
+  }
+
+  async presentToast(message:string) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 1500
+    });
+    toast.present();
   }
 }
